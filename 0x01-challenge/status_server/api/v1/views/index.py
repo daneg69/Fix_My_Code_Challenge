e33ec37004e1a2294189edcc3b52fa4e0ef7bb4f
daneg69 <85411714+daneg69@users.noreply.github.com>
2022-10-12 04:09:48 +0300

    Update index.py

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -6,7 +6,8 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+# /api/v1/status was changed to /status
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
     """
